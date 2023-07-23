e2bf784787513ea036fb9ee43859c3f2d5e0c9c1
seun-mira <130652309+seun-mira@users.noreply.github.com>
2023-07-23 20:34:05 +0100

    Updates

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,3 @@
-import { IDashboard } from './dashboard';
-import { IKey } from './keys';
-import { ILink } from './links';
-import { ITransaction } from './transaction';
-import { IMerchant, IUser } from './user';
-
 export interface Paging {
   total: number;
   per_page: number;
@@ -25,46 +19,10 @@ export interface ILoginInput {
   password: string;
 }
 
-export interface ICreateAccountInput {
-  confirm_password: string;
-  country: string;
-  email: string;
-  full_name: string;
-  password: string;
-  terms_and_conditions: boolean;
-}
 export interface ILoginResponse extends IResponseResult {
-  user: IUser;
-  merchant: IMerchant;
   token: string;
 }
 
-export interface IAuthData {
-  token: string;
-  user: IUser;
-}
-export interface ICreateAccountResponse extends IResponseResult, IAuthData {}
-
-export interface ITransactionResponse extends IResponseResult {
-  transactions: ITransaction[];
-}
-
-export interface ILinkResponse extends IResponseResult {
-  links: ILink[];
-}
-
-export interface ILinkDetailResponse extends IResponseResult {
-  link: ILink;
-}
-
-export interface IKeysResponse extends IResponseResult {
-  keys: IKey[];
-}
-export interface IToggleResponse extends IResponseResult {
-  merchant: IMerchant;
-}
-
-export interface IDashboardResponse extends IResponseResult, IDashboard {}
 export type API_STATUS = 'idle' | 'pending' | 'resolved' | 'rejected';
 
 export enum API_STATE {
